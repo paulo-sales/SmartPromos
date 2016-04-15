79db77f97e3cb461c355ceb8ec2f5aea484078b1
paulo-sales <paulo.vitor@cetax.com.br>
2016-04-15 15:57:50 -0300

    função de criar cadastro parcialmente criada.

--- app/src/main/java/br/com/smartpromos/ui/activity/RegisterActivity.java
+++ app/src/main/java/br/com/smartpromos/ui/activity/RegisterActivity.java
@@ -2,26 +2,41 @@ package br.com.smartpromos.ui.activity;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.view.ViewGroup.LayoutParams;
+import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
 
 import java.util.Calendar;
 
+import br.com.smartpromos.BuildConfig;
 import br.com.smartpromos.R;
 import br.com.smartpromos.adapter.GenderAdapter;
+import br.com.smartpromos.api.general.ServiceGenerator;
+import br.com.smartpromos.api.general.SmartRepo;
+import br.com.smartpromos.api.general.request.ClienteRequest;
 import br.com.smartpromos.api.general.request.LocalizacaoRequest;
+import br.com.smartpromos.api.general.response.ClienteResponse;
+import br.com.smartpromos.api.general.response.LocalizacaoResponse;
+import br.com.smartpromos.ui.fragment.DialogUI;
+import br.com.smartpromos.util.SmartSharedPreferences;
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
 
 public class RegisterActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener  {
 
@@ -34,10 +49,41 @@ public class RegisterActivity extends AppCompatActivity implements DatePickerDia
 
     private LocalizacaoRequest localizacaoRequest;
 
+    private EditText edtNome;
+    private EditText edtSobreNome;
+    private EditText edtEmail;
+    private EditText edtCpf;
+    private EditText edtPass;
+    private EditText edtConfPass;
+    private Button btnRegister;
+
+    String nome;
+    String sobrenome;
+    String email;
+    String cpf;
+    String dataNasc;
+    String senha;
+    String confSenha;
+    String genero;
+    int idgenero;
+    ClienteRequest clienteRequest;
+
+    private SmartRepo smartRepo = ServiceGenerator.createService(SmartRepo.class, BuildConfig.REST_SERVICE_URL, 45);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+
+        edtNome = (EditText) findViewById(R.id.edtNome);
+        edtSobreNome = (EditText) findViewById(R.id.edtSobreNome);
+        edtEmail = (EditText) findViewById(R.id.edtEmail);
+        edtCpf = (EditText) findViewById(R.id.edtCpf);
+        edtPass = (EditText) findViewById(R.id.edtPass);
+        edtConfPass = (EditText) findViewById(R.id.edtConfPass);
+        btnRegister = (Button) findViewById(R.id.btnRegister);
+
 
         Intent i = getIntent();
 
@@ -94,6 +140,15 @@ public class RegisterActivity extends AppCompatActivity implements DatePickerDia
 
         });
 
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cadastrarCliente();
+            }
+        });
+
+
     }
 
     @Override
@@ -106,4 +161,157 @@ public class RegisterActivity extends AppCompatActivity implements DatePickerDia
         String date = dayOfMonth + "/" + (monthOfYear + 1) + "/" + year;
         edtDataNasc.setText(date);
     }
+
+    private boolean validarDados(){
+        nome = edtNome.getText().toString();
+        sobrenome = edtSobreNome.getText().toString();
+        email = edtEmail.getText().toString();
+        cpf = edtCpf.getText().toString();
+        dataNasc = edtDataNasc.getText().toString();
+        senha = edtPass.getText().toString();
+        confSenha = edtConfPass.getText().toString();
+        genero = spinnerGender.getSelectedItem().toString();
+        idgenero = ( !genero.equalsIgnoreCase("Gênero") && genero.equalsIgnoreCase("Masculino") ) ? 1 : 2;
+        if(nome.equals("") || nome == null){
+            return false;
+        }
+
+        if(sobrenome.equals("") || sobrenome == null){
+            return false;
+        }
+
+
+        if(email.equals("") || email == null){
+            return false;
+        }
+
+        if(cpf.equals("") || cpf == null){
+            return false;
+        }
+
+        if(dataNasc.equals("") || dataNasc == null){
+            return false;
+        }
+
+        if(senha.equals("") || senha == null){
+            return false;
+        }
+
+        if(confSenha.equals("") || confSenha == null){
+            return false;
+        }
+
+        if(genero.equals("") || genero.equalsIgnoreCase("Gênero") || genero == null){
+            return false;
+        }
+        return true;
+    }
+
+    private boolean checarSenha(){
+
+        String senha = edtPass.getText().toString();
+        String confSenha = edtConfPass.getText().toString();
+
+        if(senha.equals(confSenha)){
+            return true;
+        }
+        return false;
+    }
+
+    private void cadastrarCliente(){
+
+        if(validarDados()){
+            if(checarSenha()){
+                String[] data = dataNasc.split("/");
+                int dia = Integer.parseInt(data[0]);
+                int mes = Integer.parseInt(data[1]);
+                int ano = Integer.parseInt(data[2]);
+                int c = 0;
+                try{
+                    c = Integer.parseInt(cpf);
+                }catch (NumberFormatException e){
+                    e.printStackTrace();
+                }
+
+                ClienteRequest cliente = new ClienteRequest(c, nome, sobrenome, idgenero, dia, mes, ano, 5, 0, 1, email, senha, "");
+
+                String clienteJs = new Gson().toJson(cliente,ClienteRequest.class);
+                String localizacaoJs = new Gson().toJson(localizacaoRequest,LocalizacaoRequest.class);
+                smartRepo.criarCadastro(clienteJs,localizacaoJs, new Callback<ClienteResponse>(){
+
+
+                    @Override
+                    public void success(ClienteResponse clienteResponse, Response response) {
+                        if (clienteResponse.getMensagem().getId() == 3) {
+
+                            showDialog(clienteResponse.getMensagem().getMensagem(), "");
+                            SmartSharedPreferences.gravarUsuarioResponseCompleto(getApplicationContext(),clienteResponse);
+                            getLocale(clienteResponse.getDoc_id());
+                            startActivity(new Intent(RegisterActivity.this,DashBoardActivity.class));
+                        } else {
+
+                            showDialog("Erro ao cadastrar", clienteResponse.getMensagem().getMensagem());
+                        }
+                    }
+
+                    @Override
+                    public void failure(RetrofitError error) {
+
+                    }
+                });
+
+                Log.i("ClienteRequest",clienteJs);
+            }else{
+                showDialog("Formulário de cadastro", "Campos de senha devem ser iguais!");
+            }
+        }else{
+
+            showDialog("Formulário de cadastro", "Preencha todos os campos!");
+        }
+
+    }
+
+    public void getLocale(int doc_id){
+
+        smartRepo.getLocalizacao(doc_id, new Callback<LocalizacaoResponse>() {
+            @Override
+            public void success(LocalizacaoResponse localizacaoResponse, Response response) {
+
+                if (localizacaoResponse.getMensagem().getId() == 1) {
+
+                    SmartSharedPreferences.gravarLocalizacao(getApplicationContext(), localizacaoResponse);
+
+                }
+
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+
+            }
+        });
+
+    }
+
+    public void showDialog(String title, String descDialog) {
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        Bundle bundle = new Bundle();
+        bundle.putString("title", title);
+        bundle.putString("description", descDialog);
+
+        DialogUI newFragment = new DialogUI();
+        newFragment.setArguments(bundle);
+
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+        transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+
+        transaction.add(android.R.id.content, newFragment)
+                .addToBackStack(null).commit();
+    }
+
+
+
 }
